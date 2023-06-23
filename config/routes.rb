d90543d2f0245cc197cc91c0aab345194af9c512
yudakosuke <124324965+yudakosuke@users.noreply.github.com>
2023-06-23 04:11:41 +0000

    items

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'products/index'
+    get 'products/new'
+    get 'products/create'
+    get 'products/show'
+    get 'products/edit'
+    get 'products/update'
+  end
+  namespace :public do
+    get 'shipping_addresses/index'
+    get 'shipping_addresses/edit'
+    get 'shipping_addresses/create'
+    get 'shipping_addresses/update'
+    get 'shipping_addresses/destroy'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2023_06_21_040340) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -41,7 +40,6 @@ ActiveRecord::Schema.define(version: 2023_06_21_040340) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -68,36 +66,18 @@ ActiveRecord::Schema.define(version: 2023_06_21_040340) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "last_name", null: false
-    t.string "first_name", null: false
-    t.string "last_name_kana", null: false
-    t.string "first_name_kana", null: false
-    t.string "phone_number", null: false
-    t.string "postal_code", null: false
-    t.string "address", null: false
-    t.boolean "is_deleted", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
-
-    
-  create_table "shipping_addresses", force: :cascade do |t|
-    t.integer "customer_id"
-    t.string "name"
-    t.string "address"
-    t.string "postal_code"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
-  
   create_table "genres", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["id"], name: "index_genres_on_id"
   end
-
 
   create_table "order_products", force: :cascade do |t|
     t.integer "product_id"
@@ -133,8 +113,15 @@ ActiveRecord::Schema.define(version: 2023_06_21_040340) do
     t.index ["id"], name: "index_products_on_id"
   end
 
+  create_table "shipping_addresses", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "name"
+    t.string "address"
+    t.string "postal_code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
-
 end
