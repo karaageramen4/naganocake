69d2d0b0673c50ff0cbb29cbe26a6f73c071ec5a
EC2 Default User <ec2-user@ip-172-31-3-152.ap-northeast-1.compute.internal>
2023-06-23 23:07:39 +0000

    genres完成、cart_items途中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,22 +1,58 @@
 class Public::CartItemsController < ApplicationController
-  def show
-  end
 
-  def edit
+  before_action :authenticate_customer!
+
+  def index
+    @cart_items = current_customer.cart_items.all
+    @totalprice = 0
   end
 
   def update
+    cart_item = CartItem.find(params[:id])
+
+    cart_item.update(quantity: params[:cart_item][:quantity])
+
+    redirect_to cart_items_path
   end
 
   def destroy
+    current_customer.cart_items.find(params[:id]).destroy
+
+    redirect_to cart_items_path
   end
 
-  def destro_all
+  def destroy_all
+    current_customer.cart_items.destroy_all
+
+    redirect_to cart_items_path
   end
 
   def create
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+
+    # フォームで送った商品と同じものを探す
+    exist_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+
+    if exist_item.present?
+      exist_item.quantity += params[:cart_item][:quantity].to_i
+
+      exist_item.save
+      redirect_to cart_items_path
+    # 通常の保存処理
+    elsif @cart_item.save
+      @cart_items = current_customer.cart_items.all
+      @totalprice = 0
+      render 'index'
+    else
+      redirect_back fallback_location: root_path
+    end
   end
 
-  def index
-  end
+
+private
+
+
+    def cart_item_params
+        params.require(:cart_item).permit(:customer_id, :item_id, :quantity, :price)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,25 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'cart_items/show'
-    get 'cart_items/edit'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
-    get 'cart_items/destro_all'
-    get 'cart_items/create'
-    get 'cart_items/index'
+
+ # 管理者用
+ # URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :genres, only: [:index, :create, :edit, :update]
   end
-  namespace :public do
-    get 'cart_items/show'
-    get 'cart_items/edit'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
-    get 'cart_items/destro_all'
-    get 'cart_items/create'
-  end
-# 顧客用
-# URL /customers/sign_in ...
+
+ # 顧客用
+ # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
 
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :public do
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all', as: 'destroy_all'
+    resources :cart_items, only: [:index, :destroy, :update, :create]
+  end
+
 end
