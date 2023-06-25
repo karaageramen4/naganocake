be31261c7cf1dc2d5ee2515f684d4f25355c30cb
nakagawatomo <132278131+nakagawatomo@users.noreply.github.com>
2023-06-25 13:59:44 +0000

    追加編集2

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -3,7 +3,8 @@ class OrderProduct < ApplicationRecord
   belongs_to :order
   belongs_to :products
 
-   enum making_status: { production_not_possible: 0, production_pending: 1, in_production: 2,  production_complete: 3}
+   enum produciton_status: {"着手不可": 0,"製作待ち": 1,"製作中": 2,"製作完了": 3}
+
 
    def subtotal
     price * amount
--- config/routes.rb
+++ config/routes.rb
@@ -11,36 +11,29 @@ Rails.application.routes.draw do
   }
 
  namespace :public do
-  root to: "homes#top"
-  get '/about' => "homes#about"
-
-  get 'customers/check'
-  get 'customers/my_page', to: 'customers#show', as: 'my_page'
-  get 'customers/information/edit', to: 'customers#edit', as: 'edit_information'
-  patch 'customers/information', to: 'customers#update'
-  patch 'customers/withdraw', to: 'customers#withdraw'
-
-   resources :addresses, only: [:create, :index, :edit, :update, :destroy]
+   root to: "homes#top"
+   get '/about' => "homes#about"
+   get 'customers/check'
+   get 'customers/my_page', to: 'customers#show', as: 'my_page'
+   get 'customers/information/edit', to: 'customers#edit', as: 'edit_information'
+   patch 'customers/information', to: 'customers#update'
+   patch 'customers/withdraw', to: 'customers#withdraw'
+   resources :shipping_addresses, only: [:create, :index, :edit, :update, :destroy]
    resources :items, only: [:index, :show]
    delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
    resources :cart_items, only: [:create, :index, :update, :destroy]
-
    post '/orders/confirm' => 'orders#confirm', as: 'confirm'
-   get '/orders/complete' => 'orders#complete', as: 'complete'
-
+   get '/orders/thanks' => 'orders#thanks', as: 'thanks'
    resources :orders, only: [:new, :create, :index, :show]
-
  end
 
   namespace :admin do
-
     get '/' => "homes#top"
     resources :genres, only: [:index, :create, :edit, :update]
     resources :products, only: [:new, :create, :index, :show, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :orders, only: [:show, :update]
     resources :order_details, only: [:update]
-
   end
 
 end
