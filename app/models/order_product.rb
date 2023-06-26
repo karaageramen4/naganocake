744b440dfbaaf4169fb764d4f298a893c1c4f343
nakagawatomo <132278131+nakagawatomo@users.noreply.github.com>
2023-06-26 09:21:23 +0000

    雛形

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,13 +1,13 @@
 class OrderProduct < ApplicationRecord
 
   belongs_to :order
-  belongs_to :products
+  belongs_to :product
 
    enum produciton_status: { production_not_possible: 0, production_pending: 1, in_production: 2,  production_complete: 3}
 
 
    def subtotal
-    price * amount
+    price * quantity
    end
 
 end
