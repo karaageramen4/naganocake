2f8cece4974791494962c16580b35877e76dc273
noia1111 <132414908+noia1111@users.noreply.github.com>
2023-06-26 09:23:39 +0000

    []

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,7 +1,7 @@
 class OrderProduct < ApplicationRecord
 
   belongs_to :order
-  belongs_to :products
+  belongs_to :product
 
    enum produciton_status: { production_not_possible: 0, production_pending: 1, in_production: 2,  production_complete: 3}
 
