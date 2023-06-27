0933a2d6a9f76392dd273de6a90e0e3484836ee6
noia1111 <132414908+noia1111@users.noreply.github.com>
2023-06-27 08:40:05 +0000

    []

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -4,10 +4,16 @@ class Admin::OrderDetailsController < ApplicationController
     @order_product = OrderProduct.find(params[:id])
     @order_product.update(order_product_params)
     @order = @order_product.order
+    if @order.order_products.where(produciton_status: 2).count >= 1
+      @order.order_status = 2
+      @order.save
+    end
+
     if @order.order_products.where(produciton_status: 3).count == @order.order_products.count
       @order.order_status = 3
       @order.save
     end
+
     redirect_to request.referrer
 
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 2023_06_21_040340) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2023_06_21_040340) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
+    t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -86,7 +86,6 @@ ActiveRecord::Schema.define(version: 2023_06_21_040340) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["id"], name: "index_genres_on_id"
   end
-
 
   create_table "order_products", force: :cascade do |t|
     t.integer "product_id"
